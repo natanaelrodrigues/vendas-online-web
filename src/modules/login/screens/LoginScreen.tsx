ef556e8b4693f73cc13f04f7d7a49942d5f4564b
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2023-07-07 20:43:14 -0300

    create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from "../styles/loginScreen.styles";
+
+const LoginScreen = () => {
+    return (
+        <BackgroundImage src="./background.png" />
+    )
+}
+
+export default LoginScreen;

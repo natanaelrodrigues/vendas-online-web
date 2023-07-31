9f51ebf8c5eeb88e35841075842b139df2f4fbc6
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2023-07-31 16:39:24 -0300

    correct tootip

--- src/shared/components/tootip/tootip.style.ts
+++ src/shared/components/tootip/tootip.style.ts
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const ContainerTootip = styled.div`
-    position: relative;
-    :hover {
-        div {
-            display: block;
-        }
+  position: relative;
+  &:hover {
+    & > div {
+      display: block;
     }
-`
+  }
+`;
 export const ContainerExternal = styled.div`
-    display: none;
-    position: absolute;
-    botton: -36px;
-    padding: 4px;
-    border-radius: 4px;
-    background-color: rgba(0,0,0,0.4);
-    z-index: 10
-`
+  display: none;
+  position: absolute;
+  botton: -36px;
+  padding: 4px;
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 10;
+`;

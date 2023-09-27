18f5123d6e9410f367ef6d245c985442cf104e5a
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2023-09-27 16:13:50 -0300

    insert table user in screen

--- src/modules/user/hooks/useUser.ts
+++ src/modules/user/hooks/useUser.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRequests } from '../../../shared/hooks/useRequests';
 import { useUserReducer } from '../../../store/reducers/userReducer/useUserReducer';
 import { URL_USER_ALL } from '../../../shared/constants/urls';
@@ -6,14 +6,28 @@ import { MethodsEnum } from '../../../shared/enums/methods.enum';
 
 export const useUser = () => {
   const { users, setUsers } = useUserReducer();
+  const [usersFiltered, setUsersFiltered] = useState(users);
   const { request, loading } = useRequests();
 
   useEffect(() => {
     request(URL_USER_ALL, MethodsEnum.GET, setUsers);
   }, []);
 
+  useEffect(() => {
+    setUsersFiltered(users);
+  }, [users]);
+
+  const hendleOnChangeSearch = (value: string) => {
+    if (!value) {
+      setUsersFiltered([...users]);
+    } else {
+      setUsersFiltered([...users.filter((user) => user.name.includes(value))]);
+    }
+  };
+
   return {
-    users,
+    users: usersFiltered,
     loading,
+    hendleOnChangeSearch,
   };
 };
--- src/modules/user/screens/User.tsx
+++ src/modules/user/screens/User.tsx
@@ -1,12 +1,84 @@
+import { ColumnsType } from "antd/es/table";
 import Screen from "../../../shared/components/screen/Screen"
+import { UserType } from "../../login/types/UserType";
 import { useUser } from "../hooks/useUser";
+import { insertMaskInPhone } from "../../../shared/functions/phone";
+import { insertMaskInCpf } from "../../../shared/functions/cpf";
+import Table from "../../../shared/components/table/Table";
+import { DisplayFlexJustifyBetween, DisplayFlexJustifyCenter } from "../../../shared/components/styles/display.styled";
+import { Spin } from "antd";
+import { Input } from 'antd';
+import { LimitedContainer } from "../../../shared/components/styles/limited.styled";
+import Button from "../../../shared/components/Buttons/button/Button";
+const { Search } = Input;
+
+const columns: ColumnsType<UserType> = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'E-mail',
+        dataIndex: 'email',
+        key: 'email',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Telefone',
+        dataIndex: 'phone',
+        key: 'phone',
+        render: (text) => <a>{insertMaskInPhone(text)}</a>,
+    },
+    {
+        title: 'CPF',
+        dataIndex: 'cpf',
+        key: 'cpf',
+        render: (text) => <a>{insertMaskInCpf(text)}</a>,
+    },
+  ];
+  
 
 const User = () => {
-    const { users } = useUser();
-
-    console.log('users', users)
+    const { users, loading, hendleOnChangeSearch } = useUser();
     return (
-        <Screen>User</Screen>
+        <Screen
+        listBreadcrumb={[
+            {
+              name:'HOME'
+            },
+            {
+              name:'USUARIOS'
+            }
+          ]}>
+            {loading ? (
+                <DisplayFlexJustifyCenter>
+                    <Spin size='large'/>
+                </DisplayFlexJustifyCenter>
+            ) : ( 
+               <>
+                <DisplayFlexJustifyBetween margin='0px px 16px 0px'>
+                    <LimitedContainer width={240}>
+                        <Search placeholder='Buscar usuário' onSearch={hendleOnChangeSearch} enterButton />
+                    </LimitedContainer>     
+
+                    <LimitedContainer width={240}>
+                        <Button type="primary" onClick={() => null}>Inserir</Button>
+                    </LimitedContainer>
+   
+                </DisplayFlexJustifyBetween>
+                <Table columns={columns} dataSource={users} /> 
+               </> 
+            )}
+           
+          </Screen>
     )
 }
 
